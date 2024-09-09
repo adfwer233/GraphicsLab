84f3a606a19e5717bc8179478a4e647faa6e8c0f
Bao Anchang <ba123698745@126.com>
2024-09-09 11:59:41 +0800

    hotfix(ci): adpd cmake install option

--- cmake/fetch-glm.cmake
+++ cmake/fetch-glm.cmake
@@ -4,7 +4,7 @@ set(CMAKE_CXX_STANDARD 20)
 
 FetchContent_Declare(
     glm
-    GIT_REPOSITORY https://github.com/g-truc/glm.git
+    GIT_REPOSITORY https://github.com/adfwer233/glm.git
 )
 
 FetchContent_MakeAvailable(glm)
