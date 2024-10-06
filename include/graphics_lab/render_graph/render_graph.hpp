b4d51dc24cc82d3caf7041ea69f02b31ab6bc927
Bao Anchang <ba123698745@126.com>
2024-10-06 16:37:06 +0800

    feat: new render graph scaffold

---
+++ include/graphics_lab/render_graph/render_graph.hpp
@@ -0,0 +1,85 @@
+#pragma once
+
+/**
+ * New render graph for the graphics lab.
+ *
+ * Functionality Goals:
+ *
+ * 1. Support define or use default render graph in the imported project
+ * 2. Support computational pass for applications such as path tracing related projects
+ * 3. Support "hyper render graph", that is the composition of render graphs
+ *
+ * Design Goals:
+ *
+ * 1. Separate render graph with the rendering backend, i.e. no vulkan-related logic appears in the render graph implementation
+ *
+ * Design:
+ *
+ * We got idea from the entity component systems, pass instances and resource instances are stored in a vector `RenderGraph`
+*/
+
+#include <string>
+#include <vector>
+#include <memory>
+
+namespace GraphicsLab {
+
+namespace RenderGraph {
+
+struct RenderResource {};
+struct RenderResourceInstance{};
+
+struct RenderTexture {};
+struct RenderBuffer {};
+
+struct TextureInstance: public RenderResourceInstance{};
+struct BufferInstance: public RenderResourceInstance{};
+
+struct Pass {
+    enum class Type {
+        RenderPass,
+        ComputePass
+    };
+
+    Type type;
+    std::string name;
+    size_t index;
+    size_t instance_index;
+  private:
+
+};
+
+struct ComputePass: Pass {};
+struct RenderPass: Pass {};
+struct SwapChainPass: Pass {};
+
+struct PassInstance {};
+
+struct ComputePassInstance: public PassInstance {};
+struct RenderPassInstance: public PassInstance {};
+struct SwapChainPassInstance: public PassInstance{};
+
+struct RenderGraph {
+
+    void create_pass_instances();
+    void create_resource_instances();
+
+    void compile();
+    void render();
+
+  private:
+    std::vector<std::unique_ptr<Pass>> passes;
+    std::vector<std::unique_ptr<RenderResource>> resources;
+
+    std::vector<std::unique_ptr<RenderResourceInstance>> resource_instances;
+    std::vector<std::unique_ptr<PassInstance>> pass_instances;
+};
+
+struct HyperRenderGraph {
+    std::vector<RenderGraph*> graphs;
+
+    void render();
+};
+
+}
+}
