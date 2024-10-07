f1bad8b92fdb54fb0a9c108a6016b09c78feee57
Bao Anchang <ba123698745@126.com>
2024-10-07 14:40:08 +0800

    feat: add python script conda support

---
+++ scripts/init_toolchain.bat
@@ -0,0 +1,5 @@
+@echo off
+
+conda activate graphics
+
+cmd
--- src/main/controller/ui_states.hpp
+++ src/main/controller/ui_states.hpp
@@ -1,12 +1,13 @@
 #pragma once
 
 #include <utility>
+#include <cstdlib>
 
 #include "language/reflection/reflectors.hpp"
 #include "project/project_manager.hpp"
 #include "pybind11/embed.h"
 
-struct UIState {
+struct UIState: Reflectable {
     enum class RenderMode {
         raw,
         wireframe,
@@ -88,11 +89,48 @@ struct UIState {
     // Initialize the Python interpreter
     pybind11::scoped_interpreter python;
 
+    std::string python_interpreter_path;
+
+    ReflectDataType reflect() override {
+        return {{"python_interpreter_path", TypeErasedValue(&python_interpreter_path)}};
+    }
+
     UIState() {
         spdlog::critical("ui state constructed");
+
+        std::ifstream file("graphics_lab_state_config.json");
+        if (file.is_open()) {
+            std::stringstream buffer;
+            buffer << file.rdbuf();
+
+            json j;
+            buffer >> j;
+            spdlog::critical(j.dump());
+
+            deserialization(j);
+
+            // spdlog::info(Py_EncodeLocale(Py_GetPath(), nullptr));
+            if (not python_interpreter_path.empty()) {
+                // wchar_t *program = Py_DecodeLocale(python_interpreter_path.c_str(), nullptr);
+                //
+                // Py_SetProgramName(program);  // Optional: sets the program name (default to the binary)
+                // Py_SetPath(program);         // Set Python interpreter path
+            }
+        } else {
+            auto result = serialization();
+            std::ofstream ofstream("graphics_lab_state_config.json");
+            ofstream << result.dump(4);
+        }
+
+        // Py_Initialize();
     }
 
     ~UIState() {
+        auto result = serialization();
+        std::ofstream ofstream("graphics_lab_state_config.json");
+        ofstream << result.dump(4);
         spdlog::critical("ui state destructed");
+
+        // Py_Finalize();
     }
 };
