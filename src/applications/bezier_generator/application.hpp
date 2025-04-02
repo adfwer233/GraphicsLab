051cfd7c122a726ad4706b29f8b614f4173cf044
Bao Anchang <ba123698745@126.com>
2025-04-02 11:17:55 +0800

    feat: add bezier generation demo

---
+++ src/applications/bezier_generator/application.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "graphics_lab/graphics_lab_context.hpp"
+#include "graphics_lab/render_graph/render_context.hpp"
+
+struct BezierGeneratorApplication {
+private:
+    static constexpr int WIDTH = 1980;
+    static constexpr int HEIGHT = 1080;
+
+    GraphicsLab::GraphicsLabInternalContext appContext;
+
+public:
+    BezierGeneratorApplication() : appContext(WIDTH, HEIGHT) {};
+    ~BezierGeneratorApplication();
+
+    BezierGeneratorApplication(const BezierGeneratorApplication &) = delete;
+    BezierGeneratorApplication &operator=(const BezierGeneratorApplication &) = delete;
+
+    void run();
+};
