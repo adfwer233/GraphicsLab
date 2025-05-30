1e3faa832446104e962eb9814c0f35d8f281b234
adfwer233 <ba123698745@126.com>
2025-05-30 20:15:11 +0800

    chore: add material draft

---
+++ include/graphics_lab/material/material.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "glm/glm.hpp"
+#include "language/reflection/static_reflector.hpp"
+
+namespace GraphicsLab::Material {
+
+/**
+ * @brief Data of Principled BRDF
+ *
+ * c.f. https://disneyanimation.com/publications/physically-based-shading-at-disney/
+ */
+struct Material {
+    glm::vec3 base_color;
+    float metallic;
+    float roughness;
+    float specular;
+    float specularTint;
+    float anisotropic;
+    float sheen;
+    float sheenTint;
+    float clearcoat;
+    float clearcoatGloss;
+    float subsurface;
+};
+
+}
