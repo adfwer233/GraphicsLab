cbb6d7ec0526b2aa8de90ebfa2ebf7d362d30409
Bao Anchang <ba123698745@126.com>
2025-06-18 19:54:32 +0800

    feat: add recent model struct

---
+++ src/main/storage/recent_models.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "language/reflection/reflectors.hpp"
+#include "language/reflection/static_reflector.hpp"
+#include "platform/file_system.hpp"
+
+#include <future>
+#include <string>
+#include <vector>
+#include <filesystem>
+
+namespace GraphicsLab {
+
+struct RecentModels: Reflectable {
+
+    struct ModelData {
+        std::string name;
+        std::string path;
+
+        REFLECT(Property{"name", &ModelData::name},
+                Property{"path", &ModelData::path})
+    };
+    std::vector<ModelData> models;
+
+    void add_model() {
+        std::string path = FileSystem::chooseFile();
+        if (path != "") {
+            ModelData model;
+            std::filesystem::path filePath(path);
+            model.name = filePath.filename().string();
+            model.path = path;
+
+            models.push_back(model);
+        }
+    }
+
+    ReflectDataType reflect() override {
+        return {
+            {"add_model", TypeErasedValue(&RecentModels::add_model, this)},
+        };
+    }
+
+    REFLECT(Property{"models", &RecentModels::models})
+};
+
+}
