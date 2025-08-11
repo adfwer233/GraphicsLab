8e301fe5e24f76205c6e6ed6e394b824a9b2627c
adfwer233 <ba123698745@126.com>
2025-08-11 16:17:22 +0800

    feat: remove boost and bump di

--- cmake/fetch-boost_di.cmake
+++ cmake/fetch-boost_di.cmake
@@ -5,7 +5,7 @@ set(CMAKE_CXX_STANDARD 20)
 FetchContent_Declare(
         boost_di
         GIT_REPOSITORY https://github.com/boost-experimental/di.git
-        GIT_TAG v1.3.0
+        GIT_TAG v1.3.2
 )
 
 FetchContent_MakeAvailable(boost_di)
